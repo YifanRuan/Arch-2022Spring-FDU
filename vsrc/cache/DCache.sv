e8a19cb623277287b5397c6a59d7bd2a1f1b2704
Yifan Ruan <yfruan20@fudan.edu.cn>
2022-05-17 13:54:10 +0800

    Update DCache

--- vsrc/cache/DCache.sv
+++ vsrc/cache/DCache.sv
@@ -10,7 +10,10 @@ module DCache
 	import common::*; #(
 		/* You can modify this part to support more parameters */
 		/* e.g. OFFSET_BITS, INDEX_BITS, TAG_BITS */
-		parameter X = 1
+		parameter WORDS_PER_LINE = 16,
+        parameter ASSOCIATIVITY = 2,
+        parameter SET_NUM = 8,
+        parameter ALIGN_BYTES = 8
 	)(
 	input logic clk, reset,
 
@@ -23,6 +26,304 @@ module DCache
 `ifndef REFERENCE_CACHE
 
 	/* TODO: Lab3 Cache */
+
+    // params
+    localparam OFFSET_BITS = $clog2(WORDS_PER_LINE);
+    localparam INDEX_BITS = $clog2(SET_NUM);
+    localparam ALIGN_BITS = $clog2(ALIGN_BYTES);
+    localparam TAG_BITS = $bits(word_t) - INDEX_BITS - OFFSET_BITS - ALIGN_BITS;
+    localparam POSITION_BITS = $clog2(ASSOCIATIVITY);
+
+    localparam type offset_t = logic [OFFSET_BITS-1:0];
+    localparam type index_t = logic [INDEX_BITS-1:0];
+    localparam type tag_t = logic [TAG_BITS-1:0];
+    localparam type position_t = logic [POSITION_BITS-1:0];
+
+    function offset_t get_offset(addr_t addr);
+        return addr[OFFSET_BITS+ALIGN_BITS-1:ALIGN_BITS];
+    endfunction
+
+    function index_t get_index(addr_t addr);
+        return addr[INDEX_BITS+OFFSET_BITS+ALIGN_BITS-1:OFFSET_BITS+ALIGN_BITS];
+    endfunction
+
+    function tag_t get_tag(addr_t addr);
+        return addr[TAG_BITS+INDEX_BITS+OFFSET_BITS+ALIGN_BITS-1:INDEX_BITS+OFFSET_BITS+ALIGN_BITS];
+    endfunction
+
+    localparam type state_t = enum logic[1:0] {
+        INIT, FETCH, WRITEBACK, UNCACHED
+    };
+
+    typedef struct packed {
+        u1 valid;
+        u1 dirty;
+        tag_t tag;
+    } meta_t;
+    typedef meta_t [ASSOCIATIVITY-1:0] meta_set_t;
+    typedef logic [ASSOCIATIVITY-1:0] meta_strobe_t;
+
+    /* These info won't change in a transaction */
+    tag_t tag;
+    index_t index;
+    offset_t offset;
+    assign {tag, index, offset} = {get_tag(dreq.addr), get_index(dreq.addr), get_offset(dreq.addr)};
+
+    state_t state, state_nxt;
+    offset_t counter, counter_nxt;
+
+    // Meta RAM
+    struct packed {
+        logic en;
+        meta_strobe_t strobe;
+        meta_set_t wmeta;
+    } meta_ram;
+    meta_set_t meta_ram_rmeta;
+
+    // the RAM
+    struct packed {
+        logic    en;
+        strobe_t strobe;
+        word_t   wdata;
+    } ram;
+    word_t ram_rdata;
+
+    logic hit_data_ok;
+    position_t hit_position;
+
+    logic empty;
+    position_t empty_position;
+
+    // Choose meta
+    RAM_SinglePort #(
+		.ADDR_WIDTH(INDEX_BITS),
+		.DATA_WIDTH($bits(meta_set_t)),
+		.BYTE_WIDTH($bits(meta_t)),
+		.READ_LATENCY(0)
+    ) ram_meta (
+        .clk(clk), .en(meta_ram.en),
+        .addr(index),
+        .strobe(meta_ram.strobe),
+        .wdata(meta_ram.wmeta),
+        .rdata(meta_ram_rmeta)
+    );
+
+    // Calculate whether hit
+    always_comb begin
+        hit_data_ok = '0;
+        hit_position = '0;
+        for (int i = 0; i < ASSOCIATIVITY; ++i) begin
+            if (meta_ram_rmeta[i].tag == tag && meta_ram_rmeta[i].valid) begin
+                hit_data_ok = '1;
+                hit_position = position_t'(i);
+                break;
+            end
+        end
+        hit_data_ok &= (dreq.valid & (state == INIT));
+    end
+    // always_ff @(posedge clk) begin if (state == INIT && empty) $display("%x", dreq.addr); end
+
+    // Calculate whether empty
+    always_comb begin
+        empty = '0;
+        empty_position = '0;
+        for (int i = 0; i < ASSOCIATIVITY; ++i) begin
+            if (~meta_ram_rmeta[i].valid) begin
+                empty = '1;
+                empty_position = position_t'(i);
+                break;
+            end
+        end
+    end
+
+    // PLRU
+    // the replaced position of this algorithm won't change if not hit
+    position_t replaced_position [SET_NUM-1:0];
+    for (genvar i = 0; i < SET_NUM; ++i) begin
+        wire hit = hit_data_ok && (index == index_t'(i));
+        int age [ASSOCIATIVITY-1:0], age_nxt [ASSOCIATIVITY-1:0];
+        for (genvar j = 0; j < ASSOCIATIVITY; ++j) begin
+            always_comb begin
+                age_nxt[j] = age[j];
+                if (hit) begin
+                    age_nxt[j] = {1'b0, age_nxt[j][31:1]};
+                    if (hit_position == position_t'(j)) begin
+                        age_nxt[j][31] = 1'b1;
+                    end
+                end
+            end
+        end
+        always_comb begin
+            replaced_position[i] = '0;
+            for (int j = 0; j < ASSOCIATIVITY; ++j) begin
+                if (age[j] < age[replaced_position[i]]) begin
+                    replaced_position[i] = position_t'(j);
+                end
+            end
+            if (hit) begin
+                replaced_position[i] = hit_position;
+            end
+        end
+        always_ff @(posedge clk) begin
+            if (reset) begin
+                for (int j = 0; j < ASSOCIATIVITY; ++j) begin
+                    age[j] <= '0;
+                end
+            end else begin
+                age <= age_nxt;
+            end
+        end
+    end
+
+    position_t position; // TODO
+    always_comb begin
+        if (hit_data_ok) begin
+            position = hit_position;
+        end else
+        if (empty) begin
+            position = empty_position;
+        end else begin
+            position = replaced_position[index];
+        end
+    end
+
+    // Write meta
+    // int pos = int'(position);
+    always_comb begin
+        meta_ram = '0;
+        unique case (state)
+            INIT: if (hit_data_ok) begin
+                meta_ram.en = 1;
+                meta_ram.strobe[position] = 1'b1;
+                meta_ram.wmeta[position].valid = 1;
+                meta_ram.wmeta[position].dirty = |(dreq.strobe) | meta_ram_rmeta[position].dirty;
+                meta_ram.wmeta[position].tag = tag;
+            end
+
+            FETCH: begin
+                meta_ram.en = 1;
+                meta_ram.strobe[position] = 1'b1;
+                meta_ram.wmeta[position].valid = cresp.last;
+                meta_ram.wmeta[position].dirty = 0;
+                meta_ram.wmeta[position].tag = tag;
+            end
+
+            default: begin
+                
+            end
+        endcase
+    end
+
+    // Write data
+    always_comb begin
+        ram = '0;
+        unique case (state)
+            INIT: if (|(dreq.strobe) & hit_data_ok) begin
+                ram.en = 1;
+                ram.strobe = dreq.strobe;
+                ram.wdata = dreq.data;
+            end
+
+            FETCH: begin
+                ram.en = 1;
+                ram.strobe = '1;
+                ram.wdata = cresp.data;
+            end
+
+            default: begin
+                
+            end
+        endcase
+    end
+
+    offset_t cur;
+    assign cur = hit_data_ok ? offset : counter;
+
+    // Choose data
+    RAM_SinglePort #(
+		.ADDR_WIDTH(POSITION_BITS + INDEX_BITS + OFFSET_BITS),
+		.DATA_WIDTH($bits(word_t)),
+		.BYTE_WIDTH(8),
+		.READ_LATENCY(0)
+    ) ram_data (
+        .clk(clk), .en(ram.en),
+        .addr({position, index, cur}),
+        .strobe(ram.strobe),
+        .wdata(ram.wdata),
+        .rdata(ram_rdata)
+    );
+    // always_ff @(posedge clk) begin if (ram_wdata != 64'hcccccccc_cccccccc)$display("%x", ram_rdata); end
+
+    // the FSM
+    always_comb begin
+        state_nxt = state;
+        counter_nxt = counter;
+        unique case (state)
+            INIT: if (dreq.valid) begin
+                if (dreq.addr[31]) begin
+                    counter_nxt = 0;
+                    if (~hit_data_ok) begin
+                        if (empty | ~meta_ram_rmeta[position].dirty) begin
+                            state_nxt = FETCH;
+                        end else begin
+                            state_nxt = WRITEBACK;
+                        end
+                    end
+                end else begin
+                    state_nxt = UNCACHED;
+                end
+            end
+            FETCH: begin
+                if (cresp.ready) begin
+                    counter_nxt = counter + 1;
+                end
+                if (cresp.last) begin
+                    state_nxt = INIT;
+                end
+            end
+            WRITEBACK: begin
+                if (cresp.ready) begin
+                    counter_nxt = counter + 1;
+                end
+                if (cresp.last) begin
+                    state_nxt = FETCH;
+                end
+            end
+            UNCACHED: if (cresp.last) begin
+                state_nxt = INIT;
+            end
+            default: begin
+                
+            end
+        endcase
+    end
+
+    // DBus driver
+    wire dbus_ok = ((state == INIT) & dreq.addr[31] & hit_data_ok) | ((state == UNCACHED) & cresp.last);
+    assign dresp.addr_ok = dbus_ok;
+    assign dresp.data_ok = dbus_ok;
+    assign dresp.data = dbus_ok ? ((state == INIT) ? ram_rdata : cresp.data) : '0;
+
+    // CBus driver
+    assign creq.valid = (state != INIT) & dreq.valid;
+    assign creq.is_write = (state == WRITEBACK);
+    assign creq.size = MSIZE8; // TODO
+    assign creq.addr = (state == WRITEBACK) ? {meta_ram_rmeta[position].tag, index, 7'b0} : {tag, index, 7'b0};
+    assign creq.strobe = (state == WRITEBACK) ? '1 : '0;
+    assign creq.data = (state == UNCACHED) ? dreq.data : ram_rdata;
+    assign creq.len = (state == UNCACHED) ? MLEN1 : AXI_BURST_LEN;
+    assign creq.burst = (state == UNCACHED) ? AXI_BURST_FIXED : AXI_BURST_INCR;
+
+    // flip-flop
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            state <= INIT;
+            counter <= '0;
+        end else begin
+            state <= state_nxt;
+            counter <= counter_nxt;
+        end
+    end
 
 
 `else
